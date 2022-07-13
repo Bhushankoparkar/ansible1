157589980f2d12fb349ba9d07b115f6cad2a6931
bhushan <bhushankoparkar97@gmail.com>
2022-07-13 17:19:59 +0530

    fsfsf

---
+++ ob.groovy
@@ -0,0 +1,12 @@
+job('git_item') {
+    scm {
+        git {
+            remote {
+                name('origin')
+                url('https://github.com/Bhushankoparkar/ansible1.git')
+            }
+        }
+    }
+    steps {
+        shell('echo good morning')
+}
